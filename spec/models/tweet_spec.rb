d3b371628ade4e31a886735960e28c5483b605ba
Pete Hawkins <petey@hey.com>
2023-05-23 21:18:07 +0100

    Add like component and integrate the UI

--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -3,4 +3,5 @@ require "rails_helper"
 RSpec.describe Tweet, type: :model do
   it { is_expected.to validate_presence_of :body }
   it { is_expected.to belong_to :user }
+  it { is_expected.to have_many :likes }
 end
